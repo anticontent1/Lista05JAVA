7046cb57e75f363cd9085c7e80acf615c542ea25
Gabriel Biankati <gabriel.2023014161@alunocesul.com>
2024-06-06 22:17:04 -0300

    mudanças de pastas

--- src/test/java/br/cesul/ex02/CondominiumTest.java
+++ src/test/java/br/cesul/ex02/CondominiumTest.java
@@ -3,6 +3,7 @@ package br.cesul.ex02;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,5 +20,20 @@ public class CondominiumTest {
                 LocalDate.now());
 
         assertEquals(400D, condominium.getFee());
+    }
+
+    @Test
+    public void testCondominiumOverdue() {
+        var dueDate = LocalDate.of(2024, Month.JUNE, 1);
+
+        var condominium = new Condominium(
+                1000D,
+                3000D,
+                1500D,
+                500D,
+                15,
+                dueDate);
+
+        assertEquals(440D, condominium.getFee());
     }
 }
